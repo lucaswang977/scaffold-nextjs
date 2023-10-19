6088248881d27c3b271deb1b234beec4d5093079
Lucas Wang <118956159+lucaswang977@users.noreply.github.com>
2023-10-19 11:47:10 +0900

    some experiments

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { useEffect } from "react"
+import * as React from "react"
+import { useState } from "react"
+
+export default function Home() {
+  return (
+    <main>
+      <p className="text-xl text-red-500 ">Hello World!</p>
+    </main>
+  );
+}
---
+++ prettier.config.mjs
@@ -0,0 +1,24 @@
+const config = {
+  // Standard prettier options
+  singleQuote: true,
+  semi: true,
+  // Since prettier 3.0, manually specifying plugins is required
+  plugins: [
+    '@ianvs/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss',
+  ],
+  // This plugin's options
+  importOrder: [
+    '^@core/(.*)$',
+    '',
+    '^@server/(.*)$',
+    '',
+    '^@ui/(.*)$',
+    '',
+    '^[./]',
+  ],
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  importOrderTypeScriptVersion: '5.0.0',
+};
+
+export default config;
